f62e9c1f49cd8003ed8f50dc61ab93e61afbd035
KimBJStroke <Kim.BJ.Stroke@gmail.com>
2016-07-07 09:19:15 +0900

    Programming Assignment 2: Lexical Scoping
    Programming assignment 2 from R programming - Coursera (Data Science
    specialization)
    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation 
+## and there may be some benefit to caching the inverse of a matrix 
+## rather than compute it repeatedly (there are also alternatives to matrix inversion that we will not discuss here). 
+## Your assignment is to write a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## 1. makeCacheMatrix: 
+### This function creates a special "matrix" object that can cache its inverse.
+
+## 2. cacheSolve: 
+### This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+### If the inverse has already been calculated (and the matrix has not changed), 
+### then the cachesolve should retrieve the inverse from the cache.
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
+  cache <- NULL # initialize to NULL
+  set <- function(y) { # create the matrix in the working environment
+    x <<- y
+    cache <<- NULL
+  }
+  get <- function () x # getting the value of the matrix
+  setinv <- function(inverse) cache <<- inverse # invert the matrix and store in cache
+  getinv <- function() cache # get the inverted matrix from cache
+  list (set=set, get=get, setinv = setinv, getinv=getinv)
 
 }
 
@@ -11,5 +30,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    cache <- x$getinv() # retreive the inverted matrix from stored cache
+    if(!is.null(cache)) { # retrun inverted matrix from cache or create the matrix in the working sapce
+      message("retrieving cached data.")
+      return(cache) # display matrix
+    }
+    matrix <- x$get() # creating matrix as NULL in cache
+    cache <- solve(matrix)
+    x$setinv(cache)
+    return(cache)
+  }
+
+
+
